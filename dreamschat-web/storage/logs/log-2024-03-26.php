873a10db22a69b7bd773432d1d6f95c09e33d931
Coderlor <0850070072@sv.hcmunre.edu.cn>
2024-03-26 20:54:58 +0700

    dreamschat-web commit

---
+++ dreamschat-web/storage/logs/log-2024-03-26.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2024-03-26 15:36:47 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:37:13 --> Severity: Warning --> Undefined array key "1_week" D:\Xampp\htdocs\dreamschat\dreamschat-web\application\views\home.php 529
+ERROR - 2024-03-26 15:37:13 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:37:24 --> Severity: Warning --> Undefined array key "1_week" D:\Xampp\htdocs\dreamschat\dreamschat-web\application\views\home.php 529
+ERROR - 2024-03-26 15:37:24 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:37:29 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:38:03 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:38:14 --> Severity: Warning --> Undefined array key "1_day" D:\Xampp\htdocs\dreamschat\dreamschat-web\application\views\call.php 189
+ERROR - 2024-03-26 15:38:14 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:38:19 --> Severity: Warning --> Undefined array key "1_week" D:\Xampp\htdocs\dreamschat\dreamschat-web\application\views\home.php 529
+ERROR - 2024-03-26 15:38:19 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:38:47 --> 404 Page Not Found: Assets/img
+ERROR - 2024-03-26 15:39:00 --> Severity: Warning --> Undefined array key "1_week" D:\Xampp\htdocs\dreamschat\dreamschat-web\application\views\home.php 529
+ERROR - 2024-03-26 15:39:00 --> 404 Page Not Found: Assets/img
